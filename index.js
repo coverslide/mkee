27c1dbcc5e1c8685e6e4c8b66e30b179797156bf
Richard Hoffman <coverslide@gmail.com>
2013-04-09 09:47:26 -0700

    Require util instead of inherits. util usually gets required anyway so using inherits doesn't save my anything usually

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var EventEmitter = require('events').EventEmitter
-var inherits = require('inherits')
+var inherits = require('util').inherits
 
 module.exports = function makeEventEmitter(ctr){
   inherits(ctr, EventEmitter)
